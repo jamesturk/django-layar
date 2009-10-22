e9004db44a66ea7b2cdb37e8bdb8c9d54d6d1749
James Turk <james.p.turk@gmail.com>
2009-10-22 16:14:50 -0400

    added required files

---
+++ README.rst
@@ -0,0 +1,33 @@
+============
+django-layar
+============
+
+Django generic view for making `Layar <http://layar.com>`_ endpoints.
+
+Provides abstract class that responds to Layar API requests in the appropriate format.  By implementing two small functions it is possible to add a layer to the Layar augmented reality application for Android and iPhone.
+
+django-layar is a project of Sunlight Labs (c) 2009.
+Written by James Turk <jturk@sunlightfoundation.com>
+
+Source: http://github.com/sunlightlabs/django-layar/
+
+Requirements
+============
+
+python >= 2.4
+
+django >= 1.0
+
+Installation
+============
+
+To install run
+
+    ``python setup.py install``
+
+which will install the application into the site-packages directory.
+
+Usage
+=====
+
+TBD
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-layar',
+    version="0.1.0",
+    package_dir={'layar': 'layar'},
+    packages=['layar'],
+    description='helper for publishing data to Layar augmented reality browser from Django',
+    author='James Turk',
+    author_email='jturk@sunlightfoundation.com',
+    license='BSD License',
+    url='http://github.com/sunlightlabs/django-layar/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
